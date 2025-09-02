081acc4542b88b67748c0e3e177e547447a35bed
Galih Virgo Giantara <themagiccraft1844@gmail.com>
2025-09-02 22:45:47 +0700

    Add files via upload

--- src/db.php
+++ src/db.php
@@ -8,15 +8,9 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 // --- SESUAIKAN DENGAN PENGATURAN DATABASE ANDA ---
-<<<<<<< HEAD
-define('DB_SERVER', 'mysql_db');
+define('DB_SERVER', 'db'); // Ganti dengan server database Anda
 define('DB_USERNAME', 'admin_hima'); // Ganti dengan username database Anda
 define('DB_PASSWORD', 'Him@war128');     // Ganti dengan password database Anda
-=======
-define('DB_SERVER', 'localhost'); // Ganti dengan server database Anda
-define('DB_USERNAME', 'root'); // Ganti dengan username database Anda
-define('DB_PASSWORD', '');     // Ganti dengan password database Anda
->>>>>>> a806d5a669174029b2dcdd5a5578b4d94d3e6805
 define('DB_NAME', 'db_quiz_lms'); // Ganti dengan nama database Anda
 // ------------------------------------------------
 
